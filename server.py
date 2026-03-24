9abe5369cd54f32a5c8e4aa83b00b727852440d3
Aaron Gai <sg2088@ilab.cs.rutgers.edu>
2026-03-24 19:36:02 -0400

    stage 1 fixed ObjectStoreService class

--- server.py
+++ server.py
@@ -8,18 +8,37 @@ import objectstore_pb2 as pb
 import objectstore_pb2_grpc as pb_grpc
 
 
-class ObjectStoreSevice:
-    def put(self, request, context):
+class ObjectStoreService:
+    def Put(self, request, context):
+        pass
+    def Get(self, request, context):
+        pass
+    def Delete(self, request, context):
+        pass
+    def Update(self, request, context):
+        pass
+    def List(self, request, context):
+        pass
+    def Reset(self, request, context):
+        pass
+    def Stats(self, request, context):
+        pass
+    def ApplyWrite(self, request, context):
         pass
 
 def main():
+    print("check point1")
     parser = argparse.ArgumentParser(description="--Listen and --Cluster flag implement")
-    parser.add_argument("--Listen")
-    parser.add_argument("--Cluster")
-    args = parser._get_args
+    parser.add_argument("--listen")
+    parser.add_argument("--cluster")
+    print("check point2")
+    args = parser.parse_args()
     
     server = grpc.server(futures.ThreadPoolExecutor())
-    pb_grpc.add_ObjectStoreServicer_to_server(ObjectStoreServicer(), server)
+    pb_grpc.add_ObjectStoreServicer_to_server(ObjectStoreService(), server)
+    print("check point3")
     server.add_insecure_port(args.listen)
     server.start()
     server.wait_for_termination()
+
+main()
